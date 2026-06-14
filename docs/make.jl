6caa39d68920252860e434d52866caf453294aea
Paulogcd <gugelmopaulo@gmail.com>
2026-06-14 12:47:39 +0200

    Typo;

--- docs/make.jl
+++ docs/make.jl
@@ -17,6 +17,6 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "github.com/Paulogcd/twowayffweights.jl",
+    repo = "github.com/Paulogcd/twowayfeweights.jl",
     devbranch = "main"
 )

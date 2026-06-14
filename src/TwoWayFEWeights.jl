ce4c02bd35dc52f6afa9ada2a4146ea1542d0b5d
Paulogcd <gugelmopaulo@gmail.com>
2026-06-14 10:04:55 +0200

    Creating main file with correct name.

---
+++ src/TwoWayFEWeights.jl
@@ -0,0 +1,70 @@
+"""
+    TwowayFEWeights
+
+Julia implementation of two-way fixed effects weight diagnostics.
+"""
+module TwoWayFEWeights
+
+    using Random
+    using DataFrames
+    using RCall
+    using Test
+    using Statistics
+    using OrderedCollections
+    using CategoricalArrays
+    using LinearAlgebra
+    using FixedEffectModels
+    using StatsBase
+    using Missings
+    using ShiftedArrays
+    using ReadStatTables
+    using PrettyTables
+    using Crayons
+
+    # Util functions
+    begin
+        include("utils_extra.jl")
+        # export(weighted_mean)
+        
+        include("utils_1_renames.jl")
+        # export(fn_ctrl_rename)
+        # export(get_controls_rename)
+        # export(fn_treatment_rename)
+        # export(get_treatments_rename)
+        # export(fn_treatment_weight_rename)
+        # export(fn_random_weight_rename)
+        # export(get_random_weight_rename)
+    
+        include("utils_2_twfe_rename.jl")
+        # export(twowayfeweights_rename_var)
+
+        include("twowayfeweights_normalize_var.jl")
+        # export(twowayfeweights_transform)
+
+        include("utils_3_twfe_transform.jl")
+        # export(twowayfeweights_filter)
+
+        include("utils_4_twfe_filter.jl")
+        # export(twowayfeweights_summarize_weights)
+
+        include("utils_5_twfe_summarize_weights.jl")
+        # export(twowayfeweights_summarize_weights)
+        
+        include("utils_6_test_random_weights.jl")
+        # export(twowayfeweights_test_random_weights)
+
+        include("twowayfeweights_calculate.jl")
+        # export(twowayfeweights_calculate)
+
+        # include("twowayfeweights_f.jl")
+        include("twowayfeweights_result.jl")
+        
+        include("twowayfeweights_struct.jl")
+        include("utils_print.jl")
+        include("print.jl")
+
+        include("twowayfeweights_f.jl")
+        export(twowayfeweights)
+    end
+
+end
